a1c16b8851f595b36cfa05458e0c5fa0441e6c10
Az1yaT <jeenbek0vv.06@gmail.com>
2022-08-20 23:08:43 +0600

    fix

--- control_work1/Program.cs
+++ control_work1/Program.cs
@@ -4,7 +4,7 @@
 
 // 1. Задаем одномерный массив строк: 
 
-string[] arrString = { "hello", "2", "world", ":-)", "xD" };
+string[] arrString = { "hello", "2", "world", ":-)",};
 
 // 2. Выводим на печать исходный массив: 
 void PrintArray(string[] arrString)
