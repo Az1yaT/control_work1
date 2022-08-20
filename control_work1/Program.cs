e8c2957cbfc39f415b8dff0ff11fa3d9725e8e54
Az1yaT <jeenbek0vv.06@gmail.com>
2022-08-20 23:05:01 +0600

    We added new void method

--- control_work1/Program.cs
+++ control_work1/Program.cs
@@ -5,3 +5,14 @@
 // 1. Задаем одномерный массив строк: 
 
 string[] arrString = { "hello", "2", "world", ":-)", "xD" };
+
+// 2. Выводим на печать исходный массив: 
+void PrintArray(string[] arrString)
+{
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arrString.Length - 1) Console.Write(arrString[i] + ", ");
+        else Console.Write(arrString[i] + "]");
+    }
+}
